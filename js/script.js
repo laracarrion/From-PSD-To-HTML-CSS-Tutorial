3672e0b08f6263683588f0a1eb4d9c0e71c7a59a
Lara Carrión <laracarrion@ucm.es>
2015-11-15 16:23:44 +0100

    JS ready

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-function abrir(url) { 
-	open(url,'','top=0,left=0,width=1440,height=1700') ; 
-}; 
+$(document).ready(function(){
+
+	function abrir(url) { 
+		open(url,'','top=0,left=0,width=1440,height=1700') ; 
+	}; 
+
+});
